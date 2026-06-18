5523493f950e978ad8a60b014b97793bee80a37d
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:14:57 -0300

    Update EntregaController.java

--- src/main/java/br/senac/prjint3/controller/EntregaController.java
+++ src/main/java/br/senac/prjint3/controller/EntregaController.java
@@ -5,10 +5,12 @@ import br.senac.prjint3.service.EntregaService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/entregas")
 @Tag(name = "entregas")
+@CrossOrigin("*")    
 public class EntregaController extends AbstractCrudController<Entrega> {
 
     public EntregaController(EntregaService service) {
