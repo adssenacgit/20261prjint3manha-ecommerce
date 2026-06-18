4cf11c94d6542f1faaeebfe3bfb07d5732d8e4fd
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:14:30 -0300

    Update CategoriaController.java

--- src/main/java/br/senac/prjint3/controller/CategoriaController.java
+++ src/main/java/br/senac/prjint3/controller/CategoriaController.java
@@ -5,10 +5,12 @@ import br.senac.prjint3.service.CategoriaService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/categorias")
 @Tag(name = "categorias")
+@CrossOrigin("*")    
 public class CategoriaController extends AbstractCrudController<Categoria> {
 
     public CategoriaController(CategoriaService service) {
