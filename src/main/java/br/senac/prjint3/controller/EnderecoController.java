9df7c70b7f02141a9712f3a4dc50e412af1b2c0e
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:14:49 -0300

    Update EnderecoController.java

--- src/main/java/br/senac/prjint3/controller/EnderecoController.java
+++ src/main/java/br/senac/prjint3/controller/EnderecoController.java
@@ -5,10 +5,12 @@ import br.senac.prjint3.service.EnderecoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/enderecos")
 @Tag(name = "enderecos")
+@CrossOrigin("*")    
 public class EnderecoController extends AbstractCrudController<Endereco> {
 
     public EnderecoController(EnderecoService service) {
