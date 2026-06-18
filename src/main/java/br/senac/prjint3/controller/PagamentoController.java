0da8c6bdf0f237049d802c5230ec3d74008ec970
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:15:15 -0300

    Update PagamentoController.java

--- src/main/java/br/senac/prjint3/controller/PagamentoController.java
+++ src/main/java/br/senac/prjint3/controller/PagamentoController.java
@@ -5,10 +5,12 @@ import br.senac.prjint3.service.PagamentoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/pagamentos")
 @Tag(name = "pagamentos")
+@CrossOrigin("*")    
 public class PagamentoController extends AbstractCrudController<Pagamento> {
 
     public PagamentoController(PagamentoService service) {
