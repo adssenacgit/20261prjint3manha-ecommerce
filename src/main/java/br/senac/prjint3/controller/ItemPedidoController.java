6285c398680499ccbc1186df537511c1e9c1d80c
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:15:05 -0300

    Update ItemPedidoController.java

--- src/main/java/br/senac/prjint3/controller/ItemPedidoController.java
+++ src/main/java/br/senac/prjint3/controller/ItemPedidoController.java
@@ -5,10 +5,12 @@ import br.senac.prjint3.service.ItemPedidoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/itens-pedido")
 @Tag(name = "itens-pedido")
+@CrossOrigin("*")    
 public class ItemPedidoController extends AbstractCrudController<ItemPedido> {
 
     public ItemPedidoController(ItemPedidoService service) {
