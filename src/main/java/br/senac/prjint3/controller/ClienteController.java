6f52c2d3ce787027178b2baaea6ce69ff2310bfb
adssenacgit <104033240+adssenacgit@users.noreply.github.com>
2026-06-18 16:14:41 -0300

    Update ClienteController.java

--- src/main/java/br/senac/prjint3/controller/ClienteController.java
+++ src/main/java/br/senac/prjint3/controller/ClienteController.java
@@ -5,10 +5,12 @@ import br.senac.prjint3.service.ClienteService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/clientes")
 @Tag(name = "clientes")
+@CrossOrigin("*")    
 public class ClienteController extends AbstractCrudController<Cliente> {
 
     public ClienteController(ClienteService service) {
